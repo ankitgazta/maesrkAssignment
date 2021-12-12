2964924f866cb46671ab705d0bc6799c49c1e646
ankitgazta <ankitgazta7@gmail.com>
2021-12-13 00:38:03 +0530

    adding the UI automation part to git........

---
+++ maesrk/src/main/java/pageObjects/ConfirmationPage.java
@@ -0,0 +1,16 @@
+package pageObjects;
+
+import baseClass.BaseTestClass;
+
+public class ConfirmationPage extends BaseTestClass {
+
+	
+	public String thankYouHeading="//div[@class='container hero-unit']/h1";
+	public String confirmationId="//table/tbody/tr[1]/td[2]";
+	
+	
+	
+	
+	
+	
+}
